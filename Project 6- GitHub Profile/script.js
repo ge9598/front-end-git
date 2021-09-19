c7bfe566238d2ae31bdd4916ce60205c7413e4fa
xiaoyan ge <cgxy1995@gmail.com>
2021-09-18 21:00:31 -0400

    Project 6 github card done

---
+++ Project 6- GitHub Profile/script.js
@@ -0,0 +1,45 @@
+const githubAPIurl = 'https://api.github.com/users/';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+async function getUser(user){
+    const response = await fetch(githubAPIurl + user);
+    const respData = await response.json();
+
+    
+    createUserCard(respData);
+}
+
+function createUserCard(user){
+
+    const cardHTML = `
+        <div class="card">
+            <div class="img-container">
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}"/>
+
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul class="info">
+                    <li>${user.followers}</li>
+                    <li>${user.following}</li>                
+                    <li>${user.public_repos}</li>
+                </ul>
+            </div>
+        </div>
+    `;
+    main.innerHTML = cardHTML;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+    if(user){
+        getUser(user);
+        search.value = "";
+    }
+});
