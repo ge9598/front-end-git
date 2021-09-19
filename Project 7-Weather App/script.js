8b2ba589e5f06c3a6f63c4b7311f139bdad84f09
xiaoyan ge <cgxy1995@gmail.com>
2021-09-19 14:13:56 -0400

    Project 7 weather app done

---
+++ Project 7-Weather App/script.js
@@ -0,0 +1,54 @@
+const APIkey = "141ecf2822e6a2a5108055316ff747a7";
+
+const APIcall = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
+const inputEl = document.getElementById('input');
+const form = document.getElementById('form');
+const main = document.getElementById('main');
+let search = '';
+async function fetchData(city){
+    await fetch(APIcall(city))
+    .then(async (data) =>{
+        if(data.ok){
+            const respData = await data.json();
+            console.log(respData);
+            writeWtoScreen(respData);
+        }
+        
+    });
+    
+
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = inputEl.value;
+    search = inputEl.value;
+    if(city){
+        fetchData(city);
+        inputEl.value = '';
+    }
+});
+
+function writeWtoScreen(data){
+    const weather = data.weather[0].description;
+    const temp = KtoC(data.main.temp);
+
+    const weatherHTML = `
+
+        <div class='weather'>
+            <h2>${temp} °C</h2
+            <small> in ${search}</small>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
+                
+        </div>
+    `;
+    main.innerHTML = weatherHTML;
+    
+
+}
+
+function KtoC(temp){
+    const cTemp = Math.round(temp - 273.15);
+    return cTemp;
+}
